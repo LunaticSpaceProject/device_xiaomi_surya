b1278e1c9bbcc54ce786038b2446ba8dcf96d9d6
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2020-11-10 20:59:24 +0530

    surya: add props to make the blur smoother

--- product_prop.mk
+++ product_prop.mk
@@ -38,21 +38,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
-    
-# LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.low_ram=false \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.log_stats=true
+    ro.sf.blurs_are_expensive=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
 
 # Smart fps switching
 PRODUCT_PRODUCT_PROPERTIES += \
